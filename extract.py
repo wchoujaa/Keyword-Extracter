4a632cf1bee8b17cbe932130530e2ad2f95a896c
wchoujaa <wchoujaa@ulb.ac.be>
2019-12-30 08:45:18 +0100

    .

--- extract.py
+++ extract.py
@@ -9,18 +9,18 @@ from tkinter import *  # from tkinter import *
 from tkinter import ttk
 from tkinter.filedialog import askopenfilename
 import wikipedia
-
+from rake_nltk import Rake
 stop_words = []
 
 f = open("french.txt", "r")
 
 for x in f:
-  stop_words.append(x.rstrip())
+    stop_words.append(x.rstrip())
 
 f.close()
 
 root = Tk()
- 
+
 
 def openPdf(filename):
     pdfFileObj = open(filename, 'rb')  # open allows you to read the file
@@ -29,9 +29,8 @@ def openPdf(filename):
     # discerning the number of pages will allow us to parse through all the pages
     num_pages = pdfReader.numPages
     count = 0
-    global text 
+    global text
     text = ""
-
 
     while count < num_pages:  # The while loop will read each page
         pageObj = pdfReader.getPage(count)
@@ -44,29 +43,28 @@ def openPdf(filename):
         text = text
     # If the above returns as False, we run the OCR library textract to #convert scanned/image based PDF files into text
 
- 
     for line in text:
         table.insert(END, line)
     warnings.filterwarnings("ignore")
 
- 
  # This is where we lauch the file manager bar.
+
+
 def OpenFile():
     name = askopenfilename(initialdir=" %UserProfile%/Desktop/",
                            filetypes=(("pdf", "*.pdf"), ("All Files", "*.*")),
                            title="Choose a file."
                            )
-     # Using try in case user types in unknown file or closes without choosing a file.
+    # Using try in case user types in unknown file or closes without choosing a file.
     openPdf(name)
- 
-
 
 
 def onClick():
     table.delete(1.0, END)
 
+
 def onKeywordClick():
-    text = table.get("1.0",END)
+    text = table.get("1.0", END)
     values = keywords(text=text, split='\n', scores=True)
 
     data = pd.DataFrame(values, columns=['keyword', 'score'])
@@ -77,9 +75,22 @@ def onKeywordClick():
             table.insert(END, x[0] + "\n")
     table.pack()
 
+
+def onSentencesClick():
+    text = table.get("1.0", END)
+    r = Rake()
+    r.extract_keywords_from_text(text)
+    values = r.get_ranked_phrases()
+    table.delete(1.0, END)
+    for x in values:
+        if(x not in stop_words):
+            table.insert(END, x + "\n")
+    table.pack()
+
+
 root.geometry("500x600")
 
-## display title
+# display title
 Title = root.title("File Opener")
 label = ttk.Label(root, text="Keyword Extractor",
                   foreground="red", font=("Helvetica", 16))
@@ -93,13 +104,14 @@ file.add_command(label='Open', command=OpenFile)
 file.add_command(label='Exit', command=lambda: exit())
 menu.add_cascade(label='File', menu=file)
 
-## display button
+# display button
 clearB = Button(root, text="clear", command=onClick)
 clearB.pack(side=BOTTOM)
 keywordB = Button(root, text="keyword", command=onKeywordClick)
 keywordB.pack(side=BOTTOM)
-
-## display text area
+keywordB = Button(root, text="sentences", command=onSentencesClick)
+keywordB.pack(side=BOTTOM)
+# display text area
 table = Text(root)
 for x in range(10):
     table.insert(END,  "\n")
